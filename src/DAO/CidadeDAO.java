d6220d591f9139424bc38fe2416c202945c71004
andreimileto <andreimileto@hotmail.com>
2017-05-09 00:17:38 -0300

    V0.99

--- src/DAO/CidadeDAO.java
+++ src/DAO/CidadeDAO.java
@@ -94,25 +94,5 @@ public class CidadeDAO {
 
     }
 
-//    public ArrayList<Cidade> consultarUmRegistro(Cidade cid) {
-//        this.cid = cid;
-//        ArrayList<Cidade> cidades = new ArrayList<>();
-//        try {
-//            Statement st = ConexaoDB.conexao.createStatement();
-//            String sql = "select * from  cidade "
-//                    + "where id = '" + cid.getId()+ "'";
-//            ResultSet resultado = st.executeQuery(sql);
-//            while (resultado.next()) {
-//                Cidade cidade = new Cidade();
-//                cidade.setId(resultado.getInt("id"));
-//                cidade.setDescricao(resultado.getString("descricao"));
-//                cidade.setAtivo(resultado.getString("ativo").charAt(0));
-//                cidades.add(cidade);
-//            }
-//        } catch (Exception e) {
-//            System.out.println("Erro ao consultar cidades " + e);
-//        }
-//        return cidades;
-//
-//    }
+
 }
